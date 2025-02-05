add6dd66ee3c4826b565c4a524dba38b9172809f
Gabriel Loirat <gabriel.loirat@etu.unice.fr>
2025-02-05 12:09:33 +0100

    correction de derniere minute

--- requete_4.sql
+++ requete_4.sql
@@ -1,11 +1,11 @@
 WITH connexions AS (
     SELECT h.gare, COUNT(DISTINCT h.lignes) AS nb_lignes
-    FROM horaires h
+    FROM "reseau"."horaires" h
     GROUP BY h.gare
 )
 SELECT c.gare, c.nb_lignes, g.quais
 FROM connexions c
-JOIN gares g ON c.gare = g.nom
+JOIN "reseau"."Gares" g ON c.gare = g.nom
 WHERE c.nb_lignes BETWEEN 2 AND 5  -- Prioriser les gares avec 2 à 5 lignes existantes
 ORDER BY c.nb_lignes DESC, g.quais DESC
 LIMIT 5;
